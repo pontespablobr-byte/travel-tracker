9f0a6a176ce8952880a46f940c20e96636afe80b
pontespablobr-byte <pontes.pablo.br@gmail.com>
2026-03-25 20:30:28 -0300

    Lógica JS, atualizado

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const inputDesc = document.querySelector("#desc-despesa");
-const inputValor = document.querySelector("#valor-grinfo");
-const inputCotação = document.querySelector("#cotacao");
+const inputValor = document.querySelector("#valor-gringo");
+const inputCotacao = document.querySelector("#cotacao");
 const btnAdicionar = document.querySelector("#btn-adicionar");
 const listaDespesasDOM = document.querySelector("#lista-despesas");
-const totalBrlDOM = document.querySelector("#total-br");
+const totalBrlDOM = document.querySelector("#total-brl");
 
 let despesas = JSON.parse(localStorage.getItem("viagem_despesas")) || [];
 
 const salvarNoLocalStorage = () => {
-  // O LocalStorage só aceita textos (String).
-  // O JSON.stringify converte nosso Array em formato de texto.
-  localStorage.setItem("viagem-despesas", JSON.stringify(despesas));
+  localStorage.setItem("viagem_despesas", JSON.stringify(despesas));
 };
 
 const adicionarDespesa = () => {
-  const desc = inputDesc.ariaValueMax.trim();
+  const desc = inputDesc.value.trim();
   const valorOriginal = parseFloat(inputValor.value);
   const cotacao = parseFloat(inputCotacao.value);
   if (desc === "" || isNaN(valorOriginal) || isNaN(cotacao)) {
@@ -38,12 +36,13 @@ const adicionarDespesa = () => {
 
   inputDesc.value = "";
   inputValor.value = "";
-  inputCotação.value = "";
+  inputCotacao.value = "";
 
   atualizarTela();
+  };
 
-  btnAdicionar.addEventListener("click", adicionarDespesa);
-};
+  btnAdicionar.addEventListener('click', adicionarDespesa);
+
 
 const atualizarTela = () => {
   const htmlDaLista = despesas.map((item) => {
@@ -62,11 +61,12 @@ const atualizarTela = () => {
 
   let somaTotal = 0;
 
-  despesas.fotEach((item) => {
-    somaTotal += item.valorReal; // Equivalente a: somaTotal = somaTotal + item.valorReal
+  despesas.forEach(item => {
+    somaTotal += item.valorReal;
   });
 
   totalBrlDOM.textContent = `R$ ${somaTotal.toFixed(2)}`;
 };
 
 atualizarTela();
+
